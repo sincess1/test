9224f063964dd47113016fd45b5c629589480925
sincess <70066482+sincess1@users.noreply.github.com>
2026-08-01 16:26:14 +0900

    Refresh evening app cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='yangyang-live-v8';
+const CACHE='yangyang-live-v9';
 const CORE=['./','./index.html','./manifest.webmanifest','./icon.svg','./apple-touch-icon.png'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
